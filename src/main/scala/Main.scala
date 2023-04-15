bdfcf43b0062218d7309d19c659028e9062f6142
Amina Adewusi <amina.adewusi@sky.uk>
2023-04-15 14:41:21 +0000

    adds method signatures

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,1 +1,17 @@
-@main def sayHello: Unit = println("Hello World!")
+@main def run: Unit =
+  val input: String = ???
+  val output: String = ???
+  etl(input, output)
+
+def etl(inputFilePath: String, outputFilePath: String): Unit =
+  val extracted = extract(inputFilePath)
+  val transformed = transform(extracted)
+  load(transformed, outputFilePath)
+end etl
+
+def extract(input: String): List[String] = ???
+def transform(data: List[String]): List[String] = ???
+def load(
+    data: List[String],
+    output: String = "src/main/resources/output.txt"
+): Unit = ???

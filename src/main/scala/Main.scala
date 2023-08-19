78e62791cb3d79329138de7a66f7031fa14802d6
Amina <amina.adewusi@gmail.com>
2023-08-19 13:22:33 +0100

    adds implementation

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,10 @@
+import java.io.{File, FileWriter}
+import scala.io.Source
+import scala.util.{Using, Try}
+
 @main def run: Unit =
-  val input: String = ???
-  val output: String = ???
+  val input: String = "src/main/resources/input.txt"
+  val output: String = "src/main/resources/output.txt"
   etl(input, output)
 
 def etl(inputFilePath: String, outputFilePath: String): Unit =
@@ -9,9 +13,17 @@ def etl(inputFilePath: String, outputFilePath: String): Unit =
   load(transformed, outputFilePath)
 end etl
 
-def extract(input: String): List[String] = ???
-def transform(data: List[String]): List[String] = ???
-def load(
-    data: List[String],
-    output: String = "src/main/resources/output.txt"
-): Unit = ???
+def extract(input: String): List[String] =
+  Using.resource(Source.fromFile(input))(_.getLines.toList)
+end extract
+
+def transform(data: List[String]): List[String] =
+  data.map(_.toLowerCase)
+end transform
+
+def load(data: List[String], output: String): Unit =
+  val file = new File(output)
+  val fileWriter = new FileWriter(file)
+  fileWriter.write(data.mkString("\n"))
+  fileWriter.close()
+end load

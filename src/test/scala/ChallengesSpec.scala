916559a46dc13f4f38ddaef634684152e7cfe3ba
Amina <amina.adewusi@gmail.com>
2023-09-17 09:07:52 +0100

    adds chapter 1 challenges

---
+++ src/test/scala/ChallengesSpec.scala
@@ -0,0 +1,23 @@
+import org.scalatest._
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.should.Matchers
+import Challenges._
+
+class ChallengesSpec extends AnyFreeSpec with Matchers {
+  "calculateSum" - {}
+
+  "filterAndConvert" - {
+    "filters names with less than four characters and converts them to uppercase" in {
+      val inputNames = List("Ravi", "Akiko", "Satoshi", "Priya", "Juan", "Bola")
+      val expectedOutput = List("RAVI", "JUAN", "BOLA")
+      val result = filterAndConvert(inputNames)
+      result shouldBe expectedOutput
+    }
+
+    "returns an empty list when there are no names with less than four characters" in {
+      val inputNames = List("Michael", "Sophia", "William")
+      val result = filterAndConvert(inputNames)
+      result shouldBe empty
+    }
+  }
+}

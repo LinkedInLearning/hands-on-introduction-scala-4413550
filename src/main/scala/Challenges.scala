f9808fe0df848c01849ec2e0de642fa737d37e03
Amina <amina.adewusi@gmail.com>
2023-09-17 09:07:52 +0100

    adds chapter 1 challenges

---
+++ src/main/scala/Challenges.scala
@@ -0,0 +1,13 @@
+object Challenges {
+  /**
+   * Write unit tests using ScalaTest to ensure the correctness of this function.
+   */
+  def calculateSum(numbers: List[Int]): Int = numbers.sum
+
+  /**
+   * Implement the method, filterAndConvert, which takes a list of
+   * names, filters out the names that have a length greater than 4
+   * characters and converts the remaining names to uppercase.
+   */
+  def filterAndConvert(names: List[String]): List[String] = ???
+}
